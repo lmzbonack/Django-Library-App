6d4972b0ea42dd337c3e34ca4390d4e28dd3004b
Luc Zbonack <lmzboanck@gmail.com>
2017-12-08 16:00:45 -0700

    Add url pattern for accounts/ routes

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -30,3 +30,9 @@ urlpatterns = [
 
 # Use static() to add url mapping to serve static files during development (only)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#Add Django site authentication urls to enable login, logout, password mgmt.
+
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
