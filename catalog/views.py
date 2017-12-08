99e503c068f5725903ec928f4a6e5b19ae685392
Luc Zbonack <lmzboanck@gmail.com>
2017-12-08 10:12:35 -0700

    Use the Django sessions variable to count the number of visits to the site in an active session

--- catalog/views.py
+++ catalog/views.py
@@ -13,13 +13,17 @@ def index(request):
     #Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status='a').count()
     num_authors= Author.objects.count() #'all()' is implied by default
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     num_genres = Genre.objects.all().count()
 
     #render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres,'num_visits':num_visits},# num_visits appended
     )
 
 class BookListView(generic.ListView):
