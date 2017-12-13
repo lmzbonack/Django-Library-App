4d6e5e718b7be012f0277735ac58d5082256cd03
Luc Zbonack <lmzboanck@gmail.com>
2017-12-13 13:49:59 -0700

    import user and datetime. Add is_overdue function to BookInstance class, and add permmissions

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 #Used to generate URLs by reversing the URL patterns
 from django.urls import reverse
+#used to calculate if books are overdue
+from datetime import date
 # Required for unique book instances
 import uuid
 
@@ -54,7 +57,7 @@ class Book(models.Model):
         """
         return reverse('book-detail', args=[str(self.id)])
 
-    
+
     def display_genre(self):
         """
         Creates a string for the Genre. This is required to display genre in Admin
@@ -70,6 +73,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book',on_delete=models.SET_NULL,null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete = models.SET_NULL,null = True,blank = True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -82,6 +92,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """
