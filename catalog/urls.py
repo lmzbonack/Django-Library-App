af76f6cd63f9435cef4fa7b97d8df2ef438ee01b
Luc Zbonack <lmzboanck@gmail.com>
2017-12-14 16:21:45 -0700

    Add URL patterns to renew books, create, update, and delete authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$',views.LoanedBookByUserListView.as_view(), name='my-borrowed'),
     url(r'^borrowed/$',views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+
+urlpatterns += [
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
